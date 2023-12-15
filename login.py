80a9eca2c6c96d6528e2ae709b2b6f4e577a711b
Jimoh Yusuf <jimoh.yusuf@abujaelectricity.com>
2023-12-15 16:45:36 +0100

    login user test

---
+++ login.py
@@ -0,0 +1,13 @@
+import time
+
+user = 'admin'
+
+if user == 'admin':
+    print('You have a super user priviledge')
+    time.sleep(5)
+    print('You are welcome to the house')
+
+else:
+    print('You a guest user')
+    time.sleep(5)
+    print('Try Next Year! Thanks')
